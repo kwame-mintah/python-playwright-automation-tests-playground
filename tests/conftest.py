d6a99d4a5384b3c449081722360e7d6653350421
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2026-02-21 20:34:27 +0000

    feat(launch_web_browser): implement launching chosen browser and mocking media stream
    `launch_web_browser` and `web_page` initial fixtures to open a browser page

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import os
+import pathlib
 from datetime import datetime
 
-from pytest import Config
+import pytest
+from playwright.sync_api import sync_playwright
 
 
-def pytest_configure(config: Config) -> None:
+def pytest_configure(config: pytest.Config) -> None:
     """
     Create a `/reports/` directory and timestamp reports generated.
     """
@@ -15,10 +17,38 @@ def pytest_configure(config: Config) -> None:
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     # Build file paths
-    html_report = f"reports/report_{timestamp}.html"
-    cucumber_report = f"reports/cucumber_{timestamp}.json"
+    html_report = f"../reports/report_{timestamp}.html"
+    cucumber_report = f"../reports/cucumber_{timestamp}.json"
 
     # Inject options dynamically
     config.option.htmlpath = html_report
     config.option.cucumberjson = cucumber_report
     config.option.self_contained_html = True
+
+
+@pytest.fixture(scope="session")
+def launch_web_browser():
+    """
+    Launch the web browser ready for testing.
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            executable_path="/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+            headless=False,
+            slow_mo=1000,
+            args=[
+                "--use-fake-device-for-media-stream",
+                "--use-fake-ui-for-media-stream",
+                f"--use-file-for-fake-video-capture={pathlib.Path('data/video/pexels_arijit_dey_dog_video_15271584_3840_2160_60fps.mp4')}",
+            ],
+        )
+        yield browser
+        browser.close()
+
+
+@pytest.fixture(scope="function")
+def web_page(launch_web_browser):
+    context = launch_web_browser.new_context(permissions=["camera", "microphone"])
+    page = context.new_page()
+    yield page
+    context.close()
--- tests/test_webcam_video_stream.py
+++ tests/test_webcam_video_stream.py
@@ -1,27 +1,28 @@
-from pytest_bdd import given, then, scenario
+from playwright.sync_api import Page
+from pytest_bdd import given, then, scenario, parsers
 
 
 @scenario(
-    "features/video_stream_injection.feature",
-    "Webcam video stream is injected with mock",
+    feature_name="features/video_stream_injection.feature",
+    scenario_name="Webcam video stream is injected with mock",
 )
 def test_webcam_video_stream_is_injected_with_mock():
     """Webcam video stream is injected with mock."""
 
 
-@given("I am a user on <url>")
-def _():
+@given(name=parsers.parse("I am a user on {url}"))
+def access_given_site_url(web_page: Page, url: str):
     """I am a user on <url>."""
-    raise NotImplementedError
+    web_page.goto(url)
 
 
 @given("I grant permission for the site to access my webcam")
-def _():
+def grant_webcam_permissions(web_page: Page):
     """I grant permission for the site to access my webcam."""
     raise NotImplementedError
 
 
 @then("I should be able to view the stream on the page")
-def _():
+def view_video_stream(web_page: Page):
     """I should be able to view the stream on the page."""
     raise NotImplementedError
